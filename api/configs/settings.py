a785060cc062cfb825dbbc43564e02c377046e50
Yuro <martinelliyuri7@gmail.com>
2025-03-04 19:14:48 -0300

    wip

--- api/configs/settings.py
+++ api/configs/settings.py
@@ -19,13 +19,11 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
 # Ajusta STR_DATABASE conforme gerenciador escolhido
-if DB_SGDB == 'sqlite':  # SQLite
-    STR_DATABASE = f"sqlite:///{DB_NAME}.db"
-elif DB_SGDB == 'mysql':  # MySQL
+if DB_SGDB == 'mysql':  # MySQL
     import pymysql
     STR_DATABASE = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
 elif DB_SGDB == 'mssql':  # SQL Server
     import pymssql
     STR_DATABASE = f"mssql+pymssql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8"
-else:  # SQLite
-    STR_DATABASE = "sqlite:///apiDatabase.db"
+else:
+    STR_DATABASE = f"sqlite:///{DB_NAME}.db"
--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
 # Yuri Martinelli
-from configs.settings import *
+from db import cria_tabelas
+from configs.settings import HOST, PORT, RELOAD
 from routers.main_router import main_router
 from routers.funcionario_router import funcionario_router
 from routers.cliente_router import cliente_router
@@ -13,6 +14,8 @@ app.include_router(main_router)
 app.include_router(funcionario_router)
 app.include_router(cliente_router)
 app.include_router(produto_router)
+
+cria_tabelas()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=HOST, port=int(PORT), reload=RELOAD)
