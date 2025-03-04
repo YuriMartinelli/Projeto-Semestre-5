38d925862ba7cb6d236e65f29c2f36358bce4eb1
Yuro <martinelliyuri7@gmail.com>
2025-03-04 19:14:10 -0300

    adiciona config de banco

---
+++ api/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from configs.settings import STR_DATABASE
+
+engine = create_engine(STR_DATABASE, echo=False)
+
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+def cria_tabelas():
+    Base.metadata.create_all(engine, checkfirst=True)
